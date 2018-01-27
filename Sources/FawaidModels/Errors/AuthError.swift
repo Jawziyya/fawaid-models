86e3eb3b941c9d0cef347192de85b3ad7b0f3c98
Abdurahim Jauzee <jauzee@mail.ru>
2018-01-28 00:03:56 +0300

    Add AuthError`s

---
+++ Sources/FawaidModels/Errors/AuthError.swift
@@ -0,0 +1,13 @@
+//
+//  AuthError.swift
+//  FawaidModels
+//
+//  Created by Abdurahim Jauzee on 28/01/2018.
+//
+
+import Foundation
+
+enum AuthError: Error {
+  case invalidCredentials
+  case usernameTaken
+}
